de29157b7ffa2c39f526b1d51b206b54ed0197ba
ccrazy386 <36156504+ccrazy386@users.noreply.github.com>
2026-08-04 10:56:39 +0900

    Create requirejs-config.js

---
+++ requirejs-config.js
@@ -0,0 +1,32 @@
+var config = {
+
+    map: {
+        '*': {
+            slick :  'js/slick.min',
+            kakaoSDK: 'https://developers.kakao.com/sdk/js/kakao.js',
+            scrollmagic: 'js/scrollmagic',
+            custom_account: 'js/custom_account',
+            prototypeFix: 'js/prototype-fire-fix'
+        }
+    },
+    config: {
+        mixins: {
+            'mage/validation': {
+                'js/mage/validation-mixin': true,
+                'js/prototype-fire-fix': true
+            }
+        }
+    },
+    'shim': {
+        'slick': {
+            deps: ['jquery']
+        },
+        scrollmagic: {
+            exports: "ScrollMagic",
+          },
+    },
+    deps: [
+        'js/prototype-fire-fix',
+        'Magento_Theme/js/js-theme'
+    ]
+};
